dc3e8ae47b29c869ae2bbb912c95ff3986db4eab
Nishu singh <nishu.frontend@gmail.com>
2026-06-18 19:41:56 +0530

    Implementing Preview and Download Feature

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import { readdir, open, readFile } from "node:fs/promises";
 import http from "node:http";
+import mime from "mime-types";
+
+console.log(mime.contentType("hello.txt"));
+console.log(mime.contentType("test.mp4"));
+console.log(mime.contentType("abc.pdf"));
+console.log(mime.contentType("river.webp"));
 
 const server = http.createServer(async (req, res) => {
   if (req.url === "/favicon.ico") return res.end("No favicon");
@@ -7,7 +13,14 @@ const server = http.createServer(async (req, res) => {
     serveDirectory(req, res);
   } else {
     try {
-      const fileHandle = await open(`./storage${decodeURIComponent(req.url)}`);
+      const [url, queryString] = req.url.split("?");
+      const queryParam = {};
+      queryString.split("&").forEach((pair) => {
+        const [key, value] = pair.split("=");
+        queryParam[key] = value;
+      });
+      console.log(queryParam);
+      const fileHandle = await open(`./storage${decodeURIComponent(url)}`);
 
       const stats = await fileHandle.stat();
 
@@ -15,6 +28,14 @@ const server = http.createServer(async (req, res) => {
         serveDirectory(req, res);
       } else {
         const readStream = fileHandle.createReadStream();
+        res.setHeader("Content-Type", mime.contentType(url.slice(1)));
+        res.setHeader("Content-Length", stats.size);
+        if (queryParam.action === "download") {
+          res.setHeader(
+            "Content-Disposition",
+            `attachment; filename="${url.slice(1)}"`,
+          );
+        }
         readStream.pipe(res);
       }
     } catch (err) {
@@ -28,7 +49,8 @@ async function serveDirectory(req, res) {
   const itemsList = await readdir(`./storage${req.url}`);
   let dynamicHtml = "";
   itemsList.forEach((item) => {
-    dynamicHtml += `<a href = ".${req.url === "/" ? "" : req.url}/${item}">${item}</a><br>`;
+    dynamicHtml += `${item} <a href = ".${req.url === "/" ? "" : req.url}/${item}?action=open">Open</a>
+     <a href = ".${req.url === "/" ? "" : req.url}/${item}?action=download">Download</a><br>`;
   });
   const htmlBoilerPlate = await readFile("./boilerplate.html", "utf-8");
   res.end(htmlBoilerPlate.replace("${dynamicHtml}", dynamicHtml));
